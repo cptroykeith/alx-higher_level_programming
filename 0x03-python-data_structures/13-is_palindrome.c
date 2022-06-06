46caa904217fa4e07a75b1122cc34126a58cc1cb
cptroykeith <katongole.roy100@gmail.com>
2022-06-06 10:17:03 -0700

    1

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+/**
+ * is_palindrome_rec - check whether a singly linked list is a palindrome
+ *
+ * @left: pointer to the head node
+ * @right: the head node
+ *
+ * Return: 1 if it's a palindrome, otherwise 0 (an empty list is considered
+ * to be a palindrome)
+ *
+ */
+int is_palindrome_rec(listint_t **left, listint_t *right)
+{
+	int check1, check2;
+
+		if (right == NULL)
+					return (1);
+
+			check1 = is_palindrome_rec(left, right->next);
+				if (check1 == 0)
+							return (0);
+
+					check2 = ((*left)->n == right->n);
+						*left = (*left)->next;
+
+							return (check2);
+}
