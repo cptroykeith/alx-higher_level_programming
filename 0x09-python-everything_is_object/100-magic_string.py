86d43781dfe289865f9690fe2118912f78619dff
cptroykeith <84200675+cptroykeith@users.noreply.github.com>
2022-06-28 23:56:52 +0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string.append("Holberton")
-    return ", ".join(string)
+def magic_string(H=[]):
+    H += ["Holberton"]
+    return (", ".join(H))
