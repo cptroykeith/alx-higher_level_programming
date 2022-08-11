f22ff3ed1ce2cf3d7f8264b221a6d450499afaa0
cptroykeith <84200675+cptroykeith@users.noreply.github.com>
2022-08-11 12:52:10 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
- script that lists all priveleges of the users 'user_0d_1' and 'user_0d_2'
+-- a script that lists all priveleges of the users 'user_0d_1' and 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
