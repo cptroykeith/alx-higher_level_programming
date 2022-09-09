ae4b9146877fb6952a96dcb89ffab55a899ca537
cptroykeith <katongole.roy100@gmail.com>
2022-09-09 07:27:12 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Display a user specified state
+    Display a user specified state (safe from SQL injection)
 """
 
 from sys import argv
@@ -10,7 +10,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{}' \
-                 ORDER BY id".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
     for row in cur.fetchall():
         print(row)
